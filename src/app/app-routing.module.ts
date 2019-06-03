086276d77b85ce793246c16df1c5491915386b7e
Francois CONTE <francois.conte@odandb.com>
2019-06-03 23:52:18 +0200

    Préambule

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'detail', loadChildren: './pages/detail/detail.module#DetailPageModule' },
+  { path: 'home', loadChildren: './pages/home/home.module#HomePageModule' },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/interfaces/character.interface.ts
@@ -0,0 +1,8 @@
+export interface Character {
+    id: number;
+    name: string;
+    picture: string;
+    houses: number[];
+    seasons: number[];
+    description: string;
+}
